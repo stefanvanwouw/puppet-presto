f57c6b197be1792805553f7f86423ff05bd417b2
Stefan van Wouw <stefanvanwouw@gmail.com>
2014-04-04 10:26:02 +0000

    Update to Presto 0.63

--- manifests/presto.pp
+++ manifests/presto.pp
@@ -4,50 +4,61 @@ class presto (
 ) {
 
 
+
     file {"${install_dir}/etc/node.properties":
         owner   => 'root',
         group   => 'root',
         content => template('presto/node.properties.erb'),
+        require => File["$install_dir/etc"],
     }
 
     file {"${install_dir}/etc/log.properties":
         owner   => 'root',
         group   => 'root',
         content => template('presto/log.properties.erb'),
+        require => File["$install_dir/etc"],
     }
 
     file {"${install_dir}/etc/config.properties":
         owner   => 'root',
         group   => 'root',
         content => template('presto/config.properties.erb'),
+        require => File["$install_dir/etc"],
     }
 
     file {"${install_dir}/etc/jvm.config":
         owner   => 'root',
         group   => 'root',
         content => template('presto/jvm.config.erb'),
+        require => File["$install_dir/etc"],
     }
 
     file {"${install_dir}/etc/catalog/jmx.properties":
         owner   => 'root',
         group   => 'root',
         content => template('presto/jmx.properties.erb'),
+        require => File["$install_dir/etc"],
     }
 
     file {"${install_dir}/etc/catalog/hive.properties":
         owner   => 'root',
         group   => 'root',
         content => template('presto/hive.properties.erb'),
+        require => File["$install_dir/etc"],
     }
 
     file {$install_dir:
         ensure  => directory,
-        #source  => 'puppet:///modules/presto/presto-server-0.55',
-        #source  => 'puppet:///modules/presto/presto-server-0.56',
-        #source  => 'puppet:///modules/presto/presto-server-0.56-SNAPSHOT',
-        source  => 'puppet:///modules/presto/presto-server-0.57-SNAPSHOT',
+        source  => 'puppet:///modules/presto/presto-server-0.63',
         mode    => '0744',
         recurse => true,
+        owner   => 'root',
+        group   => 'root',
+    }
+
+    file {"$install_dir/etc":
+        ensure  => directory,
+        mode    => '0744',
         owner   => 'root',
         group   => 'root',
     }
